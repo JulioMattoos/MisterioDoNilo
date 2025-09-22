b03fa483420afe738844837200232d12afdcc3b6
Mileine Freitas <mileine.freitas29@gmail.com>
2025-09-22 20:30:58 -0300

    logica fase um

--- misteriosdonilo/SímboloArrastavel.gd
+++ misteriosdonilo/SímboloArrastavel.gd
@@ -1,11 +1,37 @@
-extends Node
+extends Sprite
 
+var valor = 1
+var arrastando = false
+var offset = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	# Tornar clicável
+	self.texture = load(get_parent().sprites_simbolos[valor])
 
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				# Começar a arrastar
+				arrastando = true
+				offset = position - get_global_mouse_position()
+				z_index = 10  # Traz para frente
+			else:
+				# Parar de arrastar
+				arrastando = false
+				z_index = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	if arrastando:
+		position = get_global_mouse_position() + offset
+		
+		# Limitar à área de jogo
+		position.x = clamp(position.x, 50, 600)
+		position.y = clamp(position.y, 100, 400)
+
+# Função para quando o símbolo é clicado (para remover)
+func _on_Símbolo_gui_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == BUTTON_RIGHT:
+			queue_free()
+			get_parent().simbolos_na_tela.erase(self)
--- misteriosdonilo/main.gd
+++ misteriosdonilo/main.gd
@@ -1,11 +1,90 @@
-extends Node
+extends Node2D
+
+# Dicionário com os paths para os sprites PNG
+var sprites_simbolos = {
+	1: "res://EgyptianNumbers/1.png",
+	2: "res://EgyptianNumbers/2.png",
+	3: "res://EgyptianNumbers/3.png",
+	4: "res://EgyptianNumbers/4.png",
+	5: "res://EgyptianNumbers/5.png",
+	6: "res://EgyptianNumbers/6.png",
+	7: "res://EgyptianNumbers/7.png",
+	8: "res://EgyptianNumbers/8.png",
+	9: "res://EgyptianNumbers/9.png",
+	10: "res://EgyptianNumbers/10.png",
+	20: "res://EgyptianNumbers/20.png",
+	30: "res://EgyptianNumbers/30.png",
+	40: "res://EgyptianNumbers/40.png",
+	50: "res://EgyptianNumbers/50.png",
+	60: "res://EgyptianNumbers/60.png",
+	70: "res://EgyptianNumbers/70.png",
+	80: "res://EgyptianNumbers/80.png",
+	90: "res://EgyptianNumbers/90.png",
+	100: "res://EgyptianNumbers/100.png",
+	200: "res://EgyptianNumbers/200.png",
+	300: "res://EgyptianNumbers/300.png",
+	400: "res://EgyptianNumbers/400.png",
+	500: "res://EgyptianNumbers/500.png",
+	600: "res://EgyptianNumbers/600.png",
+	700: "res://EgyptianNumbers/700.png",
+	800: "res://EgyptianNumbers/800.png",
+	900: "res://EgyptianNumbers/900.png",
+	1000: "res://EgyptianNumbers/1000.png",
+	2000: "res://EgyptianNumbers/2000.png",
+	3000: "res://EgyptianNumbers/3000.png",
+	4000: "res://EgyptianNumbers/4000.png",
+	5000: "res://EgyptianNumbers/5000.png",
+	6000: "res://EgyptianNumbers/6000.png",
+	7000: "res://EgyptianNumbers/7000.png",
+	8000: "res://EgyptianNumbers/8000.png",
+	9000: "res://EgyptianNumbers/9000.png",
+	
+}
+
+# Variáveis do jogo
+var numero_alvo = 0
+var expressao_atual = ""
+var resultado_jogador = 0
+var simbolos_na_tela = []
+var simbolo_arrastando = null
+
+# Referências UI
+onready var label_numero_alvo = $UI/LabelNumeroAlvo
+onready var label_expressao = $UI/LabelExpressao
+onready var label_resultado = $UI/LabelResultado
+onready var area_simbolos = $AreaSímbolos
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	# Configurar a área de drop
+	area_simbolos.connect("input_event", self, "_on_area_simbolos_input")
+	iniciar_fase()
 
+func iniciar_fase():
+	limpar_simbolos()
+	label_resultado.text = ""
+	label_resultado.modulate = Color(1, 1, 1)
+	
+	# Número alvo entre 1-30 para facilitar
+	numero_alvo = randi() % 30 + 1
+	label_numero_alvo.text = "Monte: " + converter_para_egipicio(numero_alvo)
+	
+	gerar_expressao()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func gerar_expressao():
+	var num1 = randi() % 15 + 1
+	var num2 = randi() % 15 + 1
+	var operacao = ["+", "-"][randi() % 2]
+	
+	if operacao == "-" and num1 < num2:
+		var temp = num1
+		num1 = num2
+		num2 = temp
+	
+	expressao_atual = str(num1) + " " + operacao + " " + str(num2)
+	label_expressao.text = "Calcule: " + expressao_atual
+	
+	if operacao == "+":
+		resultado_jogador = num1 + num2
+	else:
+		resultado_jogador = num1 - num2
